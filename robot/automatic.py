c24799e22fb7a79cec5af33ab3caa5b453ec0aa9
A-S-31 <121859285+A-S-31@users.noreply.github.com>
2025-04-13 16:39:22 +0530

    automatic code added

--- robot/automatic.py
+++ robot/automatic.py
@@ -14,6 +14,14 @@ motor_B_backward = 25 # Input 4 for Motor B
 TRIG= 6
 ECHO = 5
 
+TRIGL= 10
+ECHOL=9
+
+TRIGR=26
+ECHOR=16
+
+
+
 
 
 # Constants
@@ -35,6 +43,12 @@ GPIO.setup(motor_B_backward, GPIO.OUT)
 # Ultrasonic Sensor Setup
 GPIO.setup(TRIG, GPIO.OUT)
 GPIO.setup(ECHO, GPIO.IN)
+
+GPIO.setup(TRIGL, GPIO.OUT)
+GPIO.setup(ECHOL, GPIO.IN)
+
+GPIO.setup(TRIGR, GPIO.OUT)
+GPIO.setup(ECHOR, GPIO.IN)
 
 # PWM Setup for Motor Speed Control
 pwm_A = GPIO.PWM(motor_A_enable, 100)  # 100Hz frequency
@@ -96,28 +110,75 @@ def distance():
     dist = (time_elapsed * 34300) / 2
     return dist
 
+def distanceL():
+    GPIO.output(TRIGL, True)
+    time.sleep(0.00001)
+    GPIO.output(TRIGL, False)
+
+    start_time = time.time()
+    stop_time = time.time()
+
+    while GPIO.input(ECHOL) == 0:
+        start_time = time.time()
+
+    while GPIO.input(ECHOL) == 1:
+        stop_time = time.time()
+
+    time_elapsed = stop_time - start_time
+    dist = (time_elapsed * 34300) / 2
+    
+    return dist
+    
+def distanceR():
+    GPIO.output(TRIGR, True)
+    time.sleep(0.00001)
+    GPIO.output(TRIGR, False)
+
+    start_time = time.time()
+    stop_time = time.time()
+
+    while GPIO.input(ECHOR) == 0:
+        start_time = time.time()
+
+    while GPIO.input(ECHOR) == 1:
+        stop_time = time.time()
+
+    time_elapsed = stop_time - start_time
+    dist = (time_elapsed * 34300) / 2
+    
+    return dist
+    
 try:
     while True:
         dist = distance()
+        distl= distanceL()
+        distr= distanceR()
+        
         print(f"Distance: {dist:.2f} cm")
 
         if dist < DISTANCE_THRESHOLD:
             print("Obstacle detected!")
             stop()
-            backward()
             time.sleep(1)
             stop()
-            direction = input("Turn Left (L) or Right (R)? ").upper()
-            if direction == 'L':
+            print("Checking first left direction and then right direction\n")
+            
+            if distl>DISTANCE_THRESHOLD and distr<DISTANCE_THRESHOLD:
                 left()
                 time.sleep(1)
                 stop()
-            elif direction == 'R':
+            elif distr>DISTANCE_THRESHOLD and distl<DISTANCE_THRESHOLD:
                 right()
                 time.sleep(1)
                 stop()
+            elif distr>DISTANCE_THRESHOLD and distl>DISTANCE_THRESHOLD:
+                left()
+                time.sleep(1)
+                stop()
             else:
-                print("Invalid input. Going straight")
+                backward()
+                time.sleep(1)
+                stop()
             forward()
         else:
             forward()
